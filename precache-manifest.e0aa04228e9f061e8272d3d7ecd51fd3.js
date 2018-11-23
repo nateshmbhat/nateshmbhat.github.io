e8ea7a2d57a3d3cd8f99ade1437fde92be699b3e
nateshmbhat <nateshmbhat1@gmail.com>
2018-11-23 11:59:35 +0530

    update ignore and build new version for git pages

---
+++ precache-manifest.e0aa04228e9f061e8272d3d7ecd51fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a6914f05afd55c7800b",
+    "url": "/static/js/main.8a6914f0.chunk.js"
+  },
+  {
+    "revision": "e8d248573aba6bdd7087",
+    "url": "/static/js/1.e8d24857.chunk.js"
+  },
+  {
+    "revision": "8a6914f05afd55c7800b",
+    "url": "/static/css/main.17aa5cad.chunk.css"
+  },
+  {
+    "revision": "5d22acd25a53d73ca799edb82d00834f",
+    "url": "/index.html"
+  }
+];
