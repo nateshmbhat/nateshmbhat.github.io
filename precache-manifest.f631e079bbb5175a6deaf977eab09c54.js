ef51db7b39df60b86d17bf567a6f0e2987a95393
nateshmbhat <nateshmbhat1@gmail.com>
2020-01-12 18:41:37 +0530

    Updates

---
+++ precache-manifest.f631e079bbb5175a6deaf977eab09c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e9edb2c2ddf3b36eebf",
+    "url": "/static/js/main.4e9edb2c.chunk.js"
+  },
+  {
+    "revision": "190ea538f835063cb42f",
+    "url": "/static/js/1.190ea538.chunk.js"
+  },
+  {
+    "revision": "4e9edb2c2ddf3b36eebf",
+    "url": "/static/css/main.a6537ab2.chunk.css"
+  },
+  {
+    "revision": "6c4d6fdc9acd756326ecd537ffea6821",
+    "url": "/index.html"
+  }
+];
